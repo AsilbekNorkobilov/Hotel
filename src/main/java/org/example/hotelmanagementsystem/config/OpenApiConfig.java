c98c9ddddd203b5b70ff0c2a3b79d1dfece50412
Casper <asilbek2001.05@gmail.com>
2024-10-11 17:13:38 +0500

    WebMvc and openApi config

---
+++ src/main/java/org/example/hotelmanagementsystem/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package org.example.hotelmanagementsystem.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Hotel management system")
+                        .description("""
+                                Place for description
+                                """)
+                        .version("1.0").contact(new Contact().name("Asilbek Norkobilov")
+                                .email( "asilbek2001.05@gmail.com").url("https://t.me/crazy_001"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
