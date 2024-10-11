09ba3f3c13022fba5fa2c01da9baf3331d4bbfed
Casper <asilbek2001.05@gmail.com>
2024-10-11 18:22:51 +0500

    User and Role repos

---
+++ src/main/java/org/example/hotelmanagementsystem/repo/RoleRepository.java
@@ -0,0 +1,11 @@
+package org.example.hotelmanagementsystem.repo;
+
+
+import org.example.hotelmanagementsystem.entity.Role;
+import org.example.hotelmanagementsystem.entity.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Role findRoleByRoleName(RoleName roleName);
+}
