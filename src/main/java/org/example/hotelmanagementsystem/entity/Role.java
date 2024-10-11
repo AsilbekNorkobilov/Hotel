d9e55ac4e7c717cccab98107be2d2e8a66f83ab0
Casper <asilbek2001.05@gmail.com>
2024-10-11 17:22:27 +0500

    Creating user entity with roles

---
+++ src/main/java/org/example/hotelmanagementsystem/entity/Role.java
@@ -0,0 +1,28 @@
+package org.example.hotelmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.hotelmanagementsystem.entity.enums.RoleName;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+    @Override
+    public String getAuthority() {
+        return roleName.name();
+    }
+}
---
+++ src/main/java/org/example/hotelmanagementsystem/entity/User.java
@@ -0,0 +1,64 @@
+package org.example.hotelmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+
+    @Email
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    private String firstName;
+    private String lastName;
+
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+
+}
