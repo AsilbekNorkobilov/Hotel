7573776854357aa575099240c33a6b392544e1b1
Casper <asilbek2001.05@gmail.com>
2024-10-15 18:13:39 +0500

    Register dto

---
+++ src/main/java/org/example/hotelmanagementsystem/dto/CodeRequestDto.java
@@ -0,0 +1,12 @@
+package org.example.hotelmanagementsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CodeRequestDto {
+    private Integer code;
+}
